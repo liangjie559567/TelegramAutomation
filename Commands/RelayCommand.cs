9f04a993f6fe06178346123480d60d0840e5a0d0
liangjie559567 <liangjianas19911130@gmail.com>
2024-11-16 21:15:54 +0800

    Fix: 修复编译错误和可空引用问题

--- Commands/RelayCommand.cs
+++ Commands/RelayCommand.cs
@@ -1,24 +1,27 @@
+using System;
+using System.Windows.Input;
+
 namespace TelegramAutomation.Commands
 {
     public class RelayCommand : ICommand
     {
         private readonly Action _execute;
-        private readonly Func<bool> _canExecute;
+        private readonly Func<bool>? _canExecute;
 
-        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        public RelayCommand(Action execute, Func<bool>? canExecute = null)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
 
-        public event EventHandler CanExecuteChanged;
+        public event EventHandler? CanExecuteChanged;
 
-        public bool CanExecute(object parameter)
+        public bool CanExecute(object? parameter)
         {
             return _canExecute?.Invoke() ?? true;
         }
 
-        public void Execute(object parameter)
+        public void Execute(object? parameter)
         {
             _execute();
         }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System.Windows;
 using TelegramAutomation.ViewModels;
 
 namespace TelegramAutomation
